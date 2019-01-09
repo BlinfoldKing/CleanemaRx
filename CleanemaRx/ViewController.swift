9739e0fb5aad3defcbfc4011454bdf9ec9ce0e14
Ganesha Danu <ganeshadanu1@gmail.com>
2019-01-09 15:05:14 +0700

    Initial Commit

---
+++ CleanemaRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanemaRx
+//
+//  Created by Ganesha Danu on 09/01/19.
+//  Copyright © 2019 Ganesha Danu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
